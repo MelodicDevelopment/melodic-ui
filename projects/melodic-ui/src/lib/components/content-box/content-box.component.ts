4b2e8878a4b1affbaccfe399e0a2899ed9196d27
Rick Hopkins <rhopkins@languagebird.com>
2024-09-14 20:55:15 -0400

    style updates

--- projects/melodic-ui/src/lib/components/content-box/content-box.component.ts
+++ projects/melodic-ui/src/lib/components/content-box/content-box.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, effect, ElementRef, inject, input, InputSignal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+export type ContentBoxType = 'default' | 'brand' | 'info' | 'success' | 'warning' | 'error';
 
 @Component({
 	selector: 'md-content-box',
@@ -8,4 +10,14 @@ import { CommonModule } from '@angular/common';
 	template: '<ng-content></ng-content>',
 	styleUrl: './content-box.component.scss'
 })
-export class MDContentBoxComponent {}
+export class MDContentBoxComponent {
+	private _elementRef: ElementRef = inject(ElementRef);
+
+	public type: InputSignal<ContentBoxType> = input<ContentBoxType>('default');
+
+	constructor() {
+		effect(() => {
+			this._elementRef.nativeElement.classList.add(this.type());
+		});
+	}
+}
