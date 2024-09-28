24e894176214637fa6f92825db89900bcebe5283
Rick Hopkins <rhopkins@languagebird.com>
2024-09-28 08:15:39 -0400

    dropdown work

--- projects/melodic-ui/src/lib/components/drop-down/drop-down.component.ts
+++ projects/melodic-ui/src/lib/components/drop-down/drop-down.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, input, InputSignal, Type } from '@angular/core';
+import { Component, ElementRef, HostListener, inject, input, InputSignal, output, OutputEmitterRef, signal, Type, WritableSignal } from '@angular/core';
 import { MDContentBoxComponent } from '../content-box/content-box.component';
 import { MDIconComponent } from '../icon/icon.component';
 
@@ -17,6 +17,33 @@ export interface IMDDropDownOption {
 	styleUrl: './drop-down.component.scss'
 })
 export class MDDropDownComponent {
+	private _elementRef: ElementRef = inject(ElementRef);
+
 	public options: InputSignal<IMDDropDownOption[]> = input.required();
 	public isMultiple: InputSignal<boolean> = input(false);
+	public placeholder: InputSignal<string> = input('');
+
+	public input: OutputEmitterRef<unknown> = output<unknown>();
+	public change: OutputEmitterRef<unknown> = output<unknown>();
+	public focus: OutputEmitterRef<void> = output<void>();
+	public blur: OutputEmitterRef<void> = output<void>();
+
+	public isActive: WritableSignal<boolean> = signal(false);
+
+	@HostListener('click', ['$event'])
+	onClick(_: MouseEvent) {
+		this.isActive.set(true);
+	}
+
+	ngOnInit(): void {
+		document.addEventListener('click', (event: MouseEvent) => {
+			if (!this._elementRef.nativeElement.contains(event.target as Node)) {
+				this.isActive.set(false);
+			}
+		});
+	}
+
+	public onChange(value: unknown): void {
+		this.change.emit(value);
+	}
 }
