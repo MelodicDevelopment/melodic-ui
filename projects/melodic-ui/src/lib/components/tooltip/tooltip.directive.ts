47956c8ddbe92577862de523481d37f2d05edc1b
Rick Hopkins <rhopkins@languagebird.com>
2024-10-13 09:31:59 -0400

    work

--- projects/melodic-ui/src/lib/components/tooltip/tooltip.directive.ts
+++ projects/melodic-ui/src/lib/components/tooltip/tooltip.directive.ts
@@ -1,61 +1,53 @@
-import {
-	ComponentRef,
-	Directive,
-	effect,
-	ElementRef,
-	HostBinding,
-	HostListener,
-	inject,
-	input,
-	InputSignal,
-	OnInit,
-	signal,
-	ViewContainerRef,
-	WritableSignal
-} from '@angular/core';
+import { ComponentRef, Directive, effect, HostListener, inject, input, InputSignal, signal, ViewContainerRef, WritableSignal } from '@angular/core';
 import { MDToolTipComponent } from './tooltip.component';
 
 @Directive({
 	standalone: true,
 	selector: '[md-tooltip]'
 })
-export class MDToolTipDirective implements OnInit {
+export class MDToolTipDirective {
 	private _viewContainerRef: ViewContainerRef = inject(ViewContainerRef);
 	private _componentRef: ComponentRef<MDToolTipComponent> | undefined;
 
-	private _active: WritableSignal<boolean> = signal(false);
-
 	public toolTip: InputSignal<string> = input.required<string>({ alias: 'md-tooltip' });
 	public toolTipTitle: InputSignal<string | undefined> = input<string | undefined>(undefined, { alias: 'md-tooltip-title' });
-	public delay: InputSignal<number> = input<number>(300, { alias: 'md-tooltip-delay' }); // Default delay of 300ms
+	public delay: InputSignal<number> = input<number>(300, { alias: 'md-tooltip-delay' }); // Default delay of 100ms
 	public disabled: InputSignal<boolean> = input<boolean>(false, { alias: 'md-tooltip-disabled' });
 
 	@HostListener('mouseover')
 	onMouseOver(): void {
+		this.show();
+
 		setTimeout(() => {
-			this._active.set(true);
+			if (!this._componentRef) {
+				return;
+			}
+
+			(this._componentRef.location.nativeElement as HTMLElement).classList.add('active');
 		}, this.delay());
 	}
 
 	@HostListener('mouseleave')
 	onMouseLeave(): void {
+		if (!this._componentRef) {
+			return;
+		}
+
+		(this._componentRef.location.nativeElement as HTMLElement).classList.remove('active');
+
 		setTimeout(() => {
-			this._active.set(false);
+			this.hide();
 		}, this.delay());
 	}
 
-	constructor() {
-		effect(() => {
-			if (this.disabled() && this._active()) {
-				//this._componentRef?.hostView.
-			}
-		});
-	}
-
-	ngOnInit(): void {
+	show(): void {
 		this._viewContainerRef.clear();
 		this._componentRef = this._viewContainerRef.createComponent<MDToolTipComponent>(MDToolTipComponent);
 		this._componentRef.setInput('tooltipText', this.toolTip());
 		this._componentRef.setInput('tooltipTitle', this.toolTipTitle());
+	}
+
+	hide(): void {
+		this._viewContainerRef.clear();
 	}
 }
