1d6c8347274f2c6d037805602ec6079bd766726e
Rick Hopkins <rhopkins@languagebird.com>
2024-12-27 15:57:11 -0500

    window scroll service update

--- projects/melodic-ui/src/lib/services/window-scroll.service.ts
+++ projects/melodic-ui/src/lib/services/window-scroll.service.ts
@@ -2,16 +2,16 @@
 
 import { Injectable, WritableSignal, signal, inject, computed, Signal } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
-import { fromEvent } from 'rxjs';
+import { fromEvent, Observable } from 'rxjs';
 import { map, share } from 'rxjs/operators';
 
-type WindowScrollPosition = { x: number; y: number };
+type ScrollPosition = { x: number; y: number };
 
 @Injectable({ providedIn: 'root' })
 export class WindowScrollService {
 	private _document: Document = inject(DOCUMENT);
 
-	public scroll: WritableSignal<WindowScrollPosition> = signal({ x: 0, y: 0 });
+	public scroll: WritableSignal<ScrollPosition> = signal({ x: 0, y: 0 });
 	public scrollX: Signal<number> = computed(() => {
 		return this.scroll().x;
 	});
@@ -25,8 +25,15 @@ export class WindowScrollService {
 				map(() => ({ x: 0, y: window.scrollY || this._document.documentElement.scrollTop })),
 				share()
 			)
-			.subscribe((position: WindowScrollPosition) => {
+			.subscribe((position: ScrollPosition) => {
 				this.scroll.set(position);
 			});
+	}
+
+	getScrollOfTargetContainer(targetContainer: HTMLElement): Observable<ScrollPosition> {
+		return fromEvent(targetContainer, 'scroll').pipe(
+			map(() => ({ x: targetContainer.scrollLeft, y: targetContainer.scrollTop })),
+			share()
+		);
 	}
 }
